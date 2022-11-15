12282e32ceb33b0e3744c18d204894c0452c1d7c
Paige Nelmes <designer@paigenelmes.com>
2022-11-14 16:19:08 -0800

    Add character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  //Character counter function on input
+  $('#tweet-text').on("input", function() {
+    const charCount = $(this).val().length;
+    const count = (140 - charCount);
+    const counter = $(".counter");
+    counter.text(count);
+    //If the counter is less than zero, turn text red
+    if (count < 0) {
+      counter.addClass("red");
+    } else {
+      counter.removeClass("red");
+    }
+  });
+});
